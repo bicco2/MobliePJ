a27975ed71eebb3c96b21c85f390319da3df0f8d
jinhobyeong <jhb_0921@naver.com>
2022-05-19 12:09:18 +0900

    add googleMap

--- MoblieTeamProject/app/src/main/java/com/tuk/myapp/MainActivity.kt
+++ MoblieTeamProject/app/src/main/java/com/tuk/myapp/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.tuk.myapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(35.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("우리집"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker, 16F))
+    }
+
+
+
+
+
 }
